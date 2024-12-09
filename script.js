69a44e7b771161964dcfbeef7ce757eaacf99fba
Tanzil <tanziishu2004@gmail.com>
2024-12-09 17:59:07 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Web Developer","Content Writer"],  // Corrected 'Strings' to 'strings'
+    typeSpeed: 100,  // Corrected 'typespeed' to 'typeSpeed'
+    backSpeed: 100,  // Corrected 'backspeed' to 'backSpeed'
+    backDelay: 1000,
+    loop: true
+});
